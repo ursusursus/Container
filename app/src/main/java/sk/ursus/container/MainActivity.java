70ec8c7f18d20fd4c74d43cb472c1f6bada62d0f
Vlastimil Brečka <vlastimil@vlastimilbrecka.sk>
2017-12-08 18:56:32 +0100

    adasdas

---
+++ app/src/main/java/sk/ursus/container/MainActivity.java
@@ -0,0 +1,16 @@
+package sk.ursus.container;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import sk.ursus.library.Foo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Foo.foo();
+    }
+}
